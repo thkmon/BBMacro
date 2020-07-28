f92bd941e4ca24b55b1909f4b976661f1075ccc8
bbmon <bb_@naver.com>
2020-07-28 17:02:06 +0900

    200728. 최종 메시지를 읽을 수 있도록 1분을 기다린 후 종료한다.

--- BBMacro/src/com/thkmon/bbmacro/common/CommonConst.java
+++ BBMacro/src/com/thkmon/bbmacro/common/CommonConst.java
@@ -1,6 +1,6 @@
 package com.thkmon.bbmacro.common;
 
 public class CommonConst {
-	public static final String version = "200727";
+	public static final String version = "200728";
 	public static int defaultDelay = 500;
 }
--- BBMacro/src/com/thkmon/bbmacro/main/BBMacro.java
+++ BBMacro/src/com/thkmon/bbmacro/main/BBMacro.java
@@ -1,11 +1,21 @@
 package com.thkmon.bbmacro.main;
 
+import com.thkmon.bbmacro.common.CommonConst;
 import com.thkmon.bbmacro.ctrl.CommandController;
+import com.thkmon.bbmacro.util.LogUtil;
 
 public class BBMacro {
 	
 	public static void main(String[] args) {
+		LogUtil.debug("BBMacro_" + CommonConst.version);
+		LogUtil.debug("");
+		
 		CommandController commCtrl = new CommandController();
 		commCtrl.runCommand();
+		
+		// 최종 메시지를 읽을 수 있도록 1분을 기다린 후 종료한다.
+		try {
+			Thread.sleep(60000);
+		} catch (Exception e) {}
 	}
 }
